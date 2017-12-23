1836973d264131481e31c387d71c86e5ddad17dd
matka <matanc4@mail.afeka.ac.il>
2017-12-24 00:33:28 +0200

    typos

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function sendText(sender, text){
         qs : {access_token: token},
         method: "POST",
         json:{
-            receipt: {id:sender},
+            recipient: {id:sender},
             message:messageData
         }
     },
