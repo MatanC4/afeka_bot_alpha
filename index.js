f18ec657fabb2b94681e274735802cb9b8ab191a
matka <matanc4@mail.afeka.ac.il>
2017-12-24 00:28:02 +0200

    enabling msg functionality

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
  * Created by matka on 23/12/2017.
  */
 
+'use strict'
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 
 const app = express()
+
+var token = "EAAeJc1IRQ9UBALDYWkF3ZCWmM5YSkow5MGShveLObeZA5ZABEan0IHTdVDKSa14hv7AK8BIoOyQCZAwBMleo5iOOtuesosksPU36ZBtpNYDXFfc6BNX0seZBpbWlLH7wfKfzziRtJkuKgovOkYQBcouKjMLegCZAXe8EasaBN3vxwZDZD"
 
 app.set('port' , (process.env.PORT || 5000))
 
@@ -27,6 +30,40 @@ app.get('/webhook', function(req,res){
     req.send("Wrong token")
 })
 
+
+app.post('/webhook/', function(req,res){
+    var messaging_events = req.body.entry[0].messaging_events
+    for (var i = 0; i< messaging_events.length; i++){
+        var event = messaging_events[i]
+        var sender =  event.sender.id
+        if(event.message && event.message.text){
+            var text = event.message.text
+            sendText(sender,"text echo: " + text.substring(0,100))
+        }
+    }
+    res,sendStatus(200)
+})
+
+function sendText(sender, text){
+    var messageData = {text:text}
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs : {access_token, token},
+        method: "POST",
+        json:{
+            receipt: {id:sender},
+            message:messageData
+        }
+    },
+    function(error, response, body) {
+        if (error){
+            console.log("sending error")
+
+        }else if(response.body.error){
+            console.log("response body error")
+        }
+    })
+}
 
 app.listen(app.get('port'),function(){
     console.log("running: port")
