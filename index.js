2b8cdbd9c4841c42b23a8b9754f3989a5bc7488f
matka <matanc4@mail.afeka.ac.il>
2017-12-24 22:05:11 +0200

    add generic message

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ app.post('/webhook/', function(req,res){
         var sender =  event.sender.id
         if(event.message && event.message.text){
             var text = event.message.text
-            sendText(sender,"text echo: " + text.substring(0,100))
+            sendGenericMessage(sender)
+                // sendText(sender,"text echo: " + text.substring(0,100))
         }
     }
     res,sendStatus(200)
@@ -61,6 +62,57 @@ function sendText(sender, text){
 
         }else if(response.body.error){
             console.log("response body error")
+        }
+    })
+}
+
+
+
+function sendGenericMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
         }
     })
 }
