b59cc49f280a4535fd67bfe25f2782c46b1e0b52
matka <matanc4@mail.afeka.ac.il>
2018-04-14 12:41:39 +0300

    Code alignment to submitted version

--- index.js
+++ index.js
@@ -69,66 +69,6 @@ function sendText(sender,text ){
 }
 
 
-/*function sendGenericMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}*/
-
-function calcEditDistance(a,b){
-    var stringA = a
-    var stringB = b
-    var insert, remove, update
-    insert = remove = function(node) { return 1 }
-    update = function(stringA, stringB) { return stringA !== stringB ? 1 : 0 }
-    var lev = editDistance.levenshtein(stringA, stringB, insert, remove, update)
-    console.log('Levenshtein', lev.distance, lev.pairs(), lev.alignment())
-    return lev.distance
-}
-
 
 app.listen(app.get('port'),function(){
     console.log("running: port")
