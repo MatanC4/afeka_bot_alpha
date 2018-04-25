87b53adb8deef726c2aa23038facbcadc81325ae
matka <matanc4@mail.afeka.ac.il>
2018-04-25 12:03:08 +0300

    nlp

--- index.js
+++ index.js
@@ -16,7 +16,17 @@ var bodyParser = require('body-parser')
 var mHelper = require('./messagesHelper.js')
 var dbHelper = require('./firebaseHelper.js')
 var _ = require('lodash')
-var nlpClient = require('./nlpHelper.js')
+//var nlpClient = require('./nlpHelper.js')
+const {Wit, log} = require('node-wit');
+
+const nlpClient = new Wit({
+    accessToken: "GLAVEUNCTYGSRIW5XH46XFITF45LP2WH",
+    logger: new log.Logger(log.DEBUG) // optional
+});/**
+ * Created by matka on 17/04/2018.
+ */
+
+
 
 
 //create express server
--- nlpHelper.js
+++ nlpHelper.js
@@ -1,12 +1,13 @@
-const {Wit, log} = require('node-wit');
+/*const {Wit, log} = require('node-wit');
 
 const nlpClient = new Wit({
     accessToken: "GLAVEUNCTYGSRIW5XH46XFITF45LP2WH",
     logger: new log.Logger(log.DEBUG) // optional
 });/**
- * Created by matka on 17/04/2018.
- */
+ //Created by matka on 17/04/2018.
+
 
 
 
 module.exports.nlpClient = nlpClient
+*/
