a3cc7550e39df0b18d18a164b7c1873379b4ad9e
matka <matanc4@mail.afeka.ac.il>
2018-05-08 21:55:04 +0300

    test2

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var convHelper = require('./conversationHelper.js')
 var _ = require('lodash')
+const Messenger = require('messenger-node');
 
 var spawn = require("child_process").spawn;
 var pythonProcess = spawn('python',['/Users/matka/Documents/school/Final project/afeka_bot_alpha/sentimentAnalysis/functions.py']);
@@ -31,7 +32,29 @@ var pythonProcess = spawn('python',['/Users/matka/Documents/school/Final project
  */
 
 
+let client_config = {
+    'page_token': 'EAAeJc1IRQ9UBAE8d9OD0qZAFI0ZBigZCGZCP03Hx7uYPi36KZCrZBlSGJZApSm8fawqSQcLQunFGtZBwtEaJsdnRLinNC34BAsISbIRXVImZBU9UURF0fTrVa5ctVrXlZCE6JPYg3NZBZBDZA1DOhEe8afbZBd2l7SOHrYwOdCbt4mTDeVhzlKKZArTbR01dYFXXndKggJgYoKqwzCq3QZDZD',
+    'app_token': '2121453228082133|7CO96VUnUv2-AbjNU1Yf08EZOlE',
+    'api_version': 'v2.11'
+}
 
+const Client = new Messenger.Client(client_config);
+
+// PSID of the user
+let psid = '1721073544644202'
+
+// profile fields to retrieve
+let fields = ['id', 'first_name', 'last_name', 'profile_pic']
+
+Client.getUserProfile(psid, fields)
+    .then(res => {
+        // log the api response
+        console.log(res);
+    })
+.catch(e)
+{
+    console.error(e);
+}
 
 //create express server
 const app = express()
